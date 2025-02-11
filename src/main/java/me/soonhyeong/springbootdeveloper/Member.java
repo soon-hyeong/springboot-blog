8fc56ba47cfd8c655154812329d9f65b3ab11091
Joo Soon-hyeong <shjoo820@naver.com>
2025-02-11 16:37:46 +0900

    feat: 각 계층 생성

---
+++ src/main/java/me/soonhyeong/springbootdeveloper/Member.java
@@ -0,0 +1,21 @@
+package me.soonhyeong.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
